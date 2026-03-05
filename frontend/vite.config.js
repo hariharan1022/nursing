a56bb4ca954408ad72feceb57f7bf5e7ef317758
mangudi181 <mangudi181@gmail.com>
2026-03-05 20:28:58 +0530

    index-page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/nursing/',
+  base: './',
   build: {
     outDir: command === 'serve' ? 'dist' : '../',
     emptyOutDir: false,
